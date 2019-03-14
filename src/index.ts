dbe6bd8ce1c94159f970f8889516e490cde8b846
Steven Jimenez <steven@stevethedev.com>
2019-03-14 11:12:13 -0400

    Updates the way the library is exported

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export * from "./options";
-export * from "./iterable";
-export * from "./result";
+import * as iter from "./iterable";
+import * as option from "./options";
+import * as result from "./result";
+
+export { option, iter, result };
+
+export default { option, iter, result };
